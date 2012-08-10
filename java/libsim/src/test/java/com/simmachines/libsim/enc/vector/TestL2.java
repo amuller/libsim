52845037784f1da0f4ac0c0af5cce1862cc0370d
jfquesada <jfquesada@jfquesada-VirtualBox.(none)>
2012-08-10 12:20:15 -0600

    added function of Francisco Test3 and Test6

--- java/libsim/src/test/java/com/simmachines/libsim/enc/vector/TestL2.java
+++ java/libsim/src/test/java/com/simmachines/libsim/enc/vector/TestL2.java
@@ -6,9 +6,29 @@ import org.junit.Test;
 
 public class TestL2 {
 
-	@Test
+	/*
+	@TesT
 	public void test() {
 		fail("Not yet implemented");
+	}
+	*/
+	
+	@Test
+	public void test3() {
+		double[] v1 = new double[] {1, 2, 3, 5, 6};
+		double[] v2 = new double[] {7, 8, 9, 0, 1};
+		double res = L2.distance(v1, v2);
+		
+		assertTrue((res - 12.569805089976535) <= 0.0001);
+	}
+	
+	@Test
+	public void test6() {
+		double[] v1 = new double[] {20, 30, 1000, 200, 3, 50};
+		double[] v2 = new double[] {10, 100, 500, 10, 1, 250};
+		double res = L2.distance(v1, v2);
+		
+		assertTrue((res - 575.4163709871314) <= 0.0001);
 	}
 
 }
