b40fbb4cc3f46c89e1d9f588d4168c08c1e830ed
Arnoldo J. Muller-Molina <arnoldoMuller@gmail.com>
2012-08-10 12:13:45 -0600

    added L2 tests

--- java/libsim/src/test/java/com/simmachines/libsim/enc/vector/TestL2.java
+++ java/libsim/src/test/java/com/simmachines/libsim/enc/vector/TestL2.java
@@ -2,13 +2,23 @@ package com.simmachines.libsim.enc.vector;
 
 import static org.junit.Assert.*;
 
+
 import org.junit.Test;
+
+import com.diapai.ramiel.asserts.Asserts;
 
 public class TestL2 {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void test1() {
+		double[] v1 = new double[]{1, 2, 3, 5, 6};
+		double[] v2 = new double[]{7, 8, 9, 0, 1};
+		double res = L2.distance(v1, v2);		
+		assertTrue((res - 12.569805089976535) <= 0.0001);
 	}
+	
+	
+	
+	
 
 }
