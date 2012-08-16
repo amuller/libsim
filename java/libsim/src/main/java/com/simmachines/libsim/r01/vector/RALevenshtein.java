38b52361021380562e59972c21ec9eeb544e5bfe
Arnoldo J. Muller-Molina <arnoldoMuller@gmail.com>
2012-08-16 14:34:00 -0600

    updated levenshtein distance comments

--- java/libsim/src/main/java/com/simmachines/libsim/r01/vector/RALevenshtein.java
+++ java/libsim/src/main/java/com/simmachines/libsim/r01/vector/RALevenshtein.java
@@ -18,24 +18,20 @@ import com.simmachines.libsim.r01.abst.AbstractIntVector;
  * character.
  * <p>
  * [Definition]
- * {@code
-	<latex>
-	
-Mathematically, the Levenshtein distance between two strings <math>a, b</math> is given by <math>\operatorname{lev}_{a,b}(|a|,|b|)</math> where
-
-:<math>\qquad\operatorname{lev}_{a,b}(i,j) = \begin{cases}
-  0 &, i=j=0 \\
-  i &, j = 0 \land i > 0 \\
-  j &, i = 0 \land j > 0 \\
-  \min \begin{cases}
-          \operatorname{lev}_{a,b}(i-1,j) + 1 \\
-          \operatorname{lev}_{a,b}(i,j-1) + 1 \\
-          \operatorname{lev}_{a,b}(i-1,j-1) + [a_i \neq b_j]
-       \end{cases} &, \text{ else}
-\end{cases}</math>
-	
-	</latex>
- * }
+ * <p>
+ * {@code <latex>
+ * 
+ * 
+ * Mathematically, the Levenshtein distance between two strings <math>a,
+ * b</math> is given by <math>\operatorname lev}_{a,b}(|a|,|b|)</math> where
+ * 
+ * :<math>\qquad\operatorname{lev}_{a,b}(i,j) = \begin{cases} 0 &, i=j=0 \\ i &,
+ * j = 0 \land i > 0 \\ j &, i = 0 \land j > 0 \\ \min \begin{cases}
+ * \operatorname{lev}_{a,b}(i-1,j) + 1 \\ \operatorname{lev}_{a,b}(i,j-1) + 1 \\
+ * \operatorname{lev}_{a,b}(i-1,j-1) + [a_i \neq b_j] \end{cases} &, \text{
+ * else} \end{cases}</math>
+ * 
+ * </latex> }
  * <p>
  * [Reference]
  * <p>
@@ -44,36 +40,65 @@ Mathematically, the Levenshtein distance between two strings <math>a, b</math> i
  * <p>
  * | 11.1 :: Distances on General Strings :: P.202 |
  * <p>
- * @see <a href="http://en.wikipedia.org/wiki/Levenshtein_distance">Wikipedia Article</a>
+ * 
+ * @see <a href="http://en.wikipedia.org/wiki/Levenshtein_distance">Wikipedia
+ *      Article</a>
  * 
  * @author Arnoldo J. Muller Molina
  * 
  */
 public class RALevenshtein extends AbstractIntVector {
-	
-	public RALevenshtein(){
-		
+	/**
+	 * Default constructor required by R-01
+	 */
+	public RALevenshtein() {
+
 	}
-	
-	public RALevenshtein(int[] vector){
+
+	/**
+	 * Copy constructor
+	 * 
+	 * @param An
+	 *            integer vector.
+	 */
+	public RALevenshtein(int[] vector) {
 		super(vector);
 	}
-		
 
-	@Override
-	public Double distance(RA<Double> other) {
-		return Double.valueOf(Levenshtein.distance(super.getVector(), ((AbstractIntVector)other).getVector()));
-	}
-	
 	/**
-	 * Clone must be implemented by all classes. 
+	 * Parse a vector from a String
+	 * 
+	 * @param String
+	 *            that contains an encoded version of the object.
+	 */
+	public RALevenshtein(String line) {
+		super(line);
+	}
+
+	/**
+	 * Calculates the Levenshtein distance between:
+	 * 
+	 * @param First
+	 *            sequence of symbols.
+	 * @param Second
+	 *            sequence of symbols.
+	 * @return The minimum number of inserts, deletes and renames required to
+	 *         convert one sequence of symbols into the other.
 	 */
 	@Override
-	public Object clone() {	
+	public Double distance(RA<Double> other) {
+		return Double.valueOf(Levenshtein.distance(super.getVector(),
+				((AbstractIntVector) other).getVector()));
+	}
+
+	/**
+	 * Clone must be implemented by all classes that implement RA.
+	 * 
+	 * @see java.lang.Object#clone()
+	 */
+	@Override
+	public Object clone() {
 		return new RAL2(cloneHelper());
 	}
-	
-
-	
 
 }
