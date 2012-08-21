a5401b02c5d7c41ad8647bf53776c1c9410b66c6
Jose Daniel Salazar Vargas <jdsalazar@jdsalazar-Inspiron-N5010.(none)>
2012-08-20 22:27:20 -0600

    Pearson Correlation Test update

--- java/libsim/src/test/java/com/simmachines/libsim/enc/vector/TestPearsonCorrelation.java
+++ java/libsim/src/test/java/com/simmachines/libsim/enc/vector/TestPearsonCorrelation.java
@@ -4,7 +4,7 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-import com.simmachines.libsim.common.CommonCheck;
+import com.simmachines.libsim.common.CommonChecks;
 
 public class TestPearsonCorrelation {
 
@@ -15,7 +15,7 @@ public class TestPearsonCorrelation {
 		double[] v1 = new double[]{8,3,4};
 		double[] v2 = new double[]{9,8,4};
 		double res = PearsonCorrelation.distance(v1, v2);
-		assertTrue(CommonCheck.Check2Doubles(res, 0.5));
+		assertTrue(CommonChecks.equals(res, 0.5));
 	}
 	
 	@Test
@@ -25,7 +25,7 @@ public class TestPearsonCorrelation {
 		double[] v1 = new double[]{17,8,11};
 		double[] v2 = new double[]{12,7,8};
 		double res = PearsonCorrelation.distance(v1, v2);
-		assertTrue(CommonCheck.Check2Doubles(res, 0.010256681389212985));
+		assertTrue(CommonChecks.equals(res, 0.010256681389212985));
 	}
 	
 	@Test
@@ -34,7 +34,7 @@ public class TestPearsonCorrelation {
 		double[] v1 = new double[]{5, 3, 7, 4, 9, 3, 4, 2};
 		double[] v2 = new double[]{2, 6, 3, 8, 4, 8, 2, 4};
 		double res = PearsonCorrelation.distance(v1, v2);
-		assertTrue(CommonCheck.Check2Doubles(res, 1.3546783878));
+		assertTrue(CommonChecks.equals(res, 1.3546783878));
 	}
 	
 	@Test
@@ -43,7 +43,7 @@ public class TestPearsonCorrelation {
 		double[] v1 = new double[]{502, 300, 60, 2539, 345, 233, 464, 3792};
 		double[] v2 = new double[]{283, 286, 376, 8263, 2737, 9273, 726, 927};
 		double res = PearsonCorrelation.distance(v1, v2);
-		assertTrue(CommonCheck.Check2Doubles(res, 0.8703377566));
+		assertTrue(CommonChecks.equals(res, 0.8703377566));
 	}
 	
 	@Test
@@ -52,6 +52,6 @@ public class TestPearsonCorrelation {
 		double[] v1 = new double[]{0.05, 3, 0.7, 0.24, 9.3, 3, 4.1, 2.003};
 		double[] v2 = new double[]{0.2, 0.06, 3, 2.8, 4.3, 3.8, 0.42, 4.9};
 		double res = PearsonCorrelation.distance(v1, v2);
-		assertTrue(CommonCheck.Check2Doubles(res, 0.7226769102));
+		assertTrue(CommonChecks.equals(res, 0.7226769102));
 	}
 }
