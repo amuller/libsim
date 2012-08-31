9361d22d224e1e916dd841f47e320ef741984058
Arnoldo J. Muller-Molina <arnoldoMuller@gmail.com>
2012-08-31 16:47:51 -0500

    updated AbstractTanimoto

--- java/libsim/src/main/java/com/simmachines/libsim/r01/set/AbstractTanimoto.java
+++ java/libsim/src/main/java/com/simmachines/libsim/r01/set/AbstractTanimoto.java
@@ -1,9 +1,18 @@
 package com.simmachines.libsim.r01.set;
 
 import com.diapai.ramiel.RA;
+import com.simmachines.libsim.enc.set.Set;
 import com.simmachines.libsim.r01.abst.AbstractSet;
 
 public abstract class AbstractTanimoto extends AbstractSet {
+	
+	public AbstractTanimoto(){
+		super();
+	}
+	
+	public AbstractTanimoto(Set s){
+		super(s);
+	}
 	
 	
 	public Double distance(RA<Double> other){
