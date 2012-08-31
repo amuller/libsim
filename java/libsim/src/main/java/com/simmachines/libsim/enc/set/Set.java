51fdbc2c43e8015ef805ca26c90cc841b961cfab
Arnoldo J. Muller-Molina <arnoldoMuller@gmail.com>
2012-08-31 16:01:02 -0500

    updated interface

--- java/libsim/src/main/java/com/simmachines/libsim/enc/set/Set.java
+++ java/libsim/src/main/java/com/simmachines/libsim/enc/set/Set.java
@@ -16,7 +16,7 @@ public interface Set {
 	 * @return a set of all objects that are members of both A and B.
 	 */
 	
-	public HashSet<Double> intersect(HashSet<Double> c1,HashSet<Double> c2);
+	public Set  intersect(Set s1);
 	
 	
 	/**
