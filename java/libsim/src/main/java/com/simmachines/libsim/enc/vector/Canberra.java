a78d7540a443a9ca793a77d260170e34be5ee4da
Jose Daniel Salazar Vargas <jdsalazar@jdsalazar-Inspiron-N5010.(none)>
2012-08-31 16:05:02 -0600

    Canberra, RACanberra and TestCanberra

---
+++ java/libsim/src/main/java/com/simmachines/libsim/enc/vector/Canberra.java
@@ -0,0 +1,56 @@
+package com.simmachines.libsim.enc.vector;
+
+import com.simmachines.libsim.common.CommonStats;
+
+
+/**
+ * [Canberra distance]
+ * <p>
+ * :: The Canberra distance is a numerical measure of the distance between pairs of points in a vector space.
+ * <p>
+ * [Brief Description]
+ * <p>
+ * The Canberra distance is a numerical measure of the distance between pairs of points in a vector space. It is mostly used for data scattered around the origin.
+ * <p>
+ * [Definition]
+ * <p>
+ 
+ 
+ The Canberra distance between two vectors \(a,b\) is given by \( \operatorname{d^{CAD}}_{a,b}(|a|,|b|) \) where:
+ <p>
+ \( d^{CAD}(a,b)=\sum \frac{|a_i-b_i|}{|a_i|+|b_i|} \)
+ 
+ 
+ * 
+ * <p>
+ * [Reference]
+ * <p>
+ * Deza, Michel Marie, Deza, Elena :: [Encyclopedia of Distances] :: Springer |
+ * 2009
+ * <p>
+ * | 17.1 :: Similarities and distances for numerical data :: P.300 |
+ * <p>
+ * @see <a href="http://en.wikipedia.org/wiki/Canberra_distance">Wikipedia Article</a>
+ * 
+ * @author Jose Daniel Salazar-Vargas
+ * 
+ */
+
+public class Canberra {
+	
+	/**
+	 * Received two double vectors and calculates the Canberra distance.
+	 * @param v1 vector number 1.
+	 * @param v2 vector number 2.
+	 * @return The Canberra distance.
+	 */
+	
+	public static double distance(double[] v1,double[] v2){
+		double sum = 0;
+		for(int i=0;i<v1.length;i++){
+			sum += CommonStats.abs(v1[i]-v2[i])/(CommonStats.abs(v1[i])+CommonStats.abs(v2[i]));
+		}
+		return sum;
+	}
+
+}
---
+++ java/libsim/src/main/java/com/simmachines/libsim/r01/vector/RACanberra.java
@@ -0,0 +1,59 @@
+package com.simmachines.libsim.r01.vector;
+
+import com.diapai.ramiel.RA;
+import com.simmachines.libsim.enc.vector.Canberra;
+import com.simmachines.libsim.r01.abst.AbstractVector;
+
+/**
+ * [Canberra distance]
+ * <p>
+ * :: The Canberra distance is a numerical measure of the distance between pairs of points in a vector space.one-line-description.
+ * <p>
+ * @see com.simmachines.libsim.enc.vector.Canberra;
+ * @author Jose Daniel Salazar-Vargas
+ */ 
+
+
+public class RACanberra extends AbstractVector{
+		/** 
+		 * Default constructor required by R-01
+		 */
+		public RACanberra(){
+				
+		}
+		
+		/**
+		 * Copy constructor
+		 * @param obj vector to copy.
+		 */ 
+		public RACanberra(double[] obj){
+				super(obj);
+		}
+		
+		/**
+		 * Parse a vector from a String
+		 * @param line String that contains an encoded version of the object. 
+		 */
+		public RACanberra(String line){
+				super(line);
+		}
+		
+		
+		/**
+		 * Clone must be implemented by all classes that implement RA. 
+		 * @see java.lang.Object#clone()
+		 */
+		public Object clone() {	
+				return new RACanberra(cloneHelper());
+		}
+		
+		/** Calculates the Canberra distance between this and other RACanberra object
+		 *  @param other RACanberra object
+		 *  @return Canberra distance
+		 */ 
+		@Override
+				public Double distance(RA<Double> other) {
+				return Canberra.distance(getVector(),((RACanberra)other).getVector());
+		}
+		
+}
