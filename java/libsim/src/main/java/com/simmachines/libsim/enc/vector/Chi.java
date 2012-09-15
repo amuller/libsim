d7b504428aec32221091d01916007890519b6c0b
Arnoldo J. Muller-Molina <arnoldoMuller@gmail.com>
2012-09-14 19:18:12 -0500

    updated comments

--- java/libsim/src/main/java/com/simmachines/libsim/enc/vector/Chi.java
+++ java/libsim/src/main/java/com/simmachines/libsim/enc/vector/Chi.java
@@ -6,31 +6,6 @@ import com.simmachines.libsim.common.CommonStats;
 
 public class Chi {
 
-	/**
-	 * [Chi distance]
-	 * <p>
-	 * :: Receives two double vectors and calculates the chi square distance
-	 * <p>
-	 * [Brief Description]
-	 * <p>
-	 * The Chi distance... 
-	 * <p>
-	 * [Definition]
-	 * <p>
-	 The Chi distance between two Vectors \(a, b \) is given by \( \operatorname{Chi_{a,b}(|a|,|b|) \) where: 
-	 * <p>
-	 * [Reference]
-	 * <p>
-	 * Deza, Michel Marie, Deza, Elena :: [Encyclopedia of Distances] :: Springer |
-	 * 2009
-	 * <p>
-	 * | 17.2 :: Relatives of Euclidean distance :: P.303 |
-	 * <p>
-	 * @see <a href="http://wikipedia-article">Wikipedia Article</a>
-	 * 
-	 * @author Juan Francisco Quesada-Brizuela author
-	 * 
-	 */
 	
 	public static double distance(double[] o1, double[] o2) {
 		double mean_v1 = CommonStats.mean(o1);
