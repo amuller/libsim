817b3cfb1ed2348f4a270ee88c1366f3f677b5c9
jfquesada <chosco34@gmail.com>
2012-09-15 16:01:50 -0600

    adding apache.common.math and mahalanobies enc

---
+++ java/libsim/src/main/java/com/simmachines/libsim/enc/vector/Mahalanobis.java
@@ -0,0 +1,25 @@
+package com.simmachines.libsim.enc.vector;
+
+/**
+ * [Mahalanobis distance]
+ * <p>
+ * :: one-line-description
+ * <p>
+ * @see com.simmachines.libsim.enc.vector.Mahalanobis;
+ * @author Juan Francisco Quesada-Brizuela author
+ */ 
+public class Mahalanobis {
+		
+		/**
+		 * Calculates the Mahalanobis distance
+		 * @param o1 vector 1
+		 * @param o2 vector 2
+		 * @return  Mahalanobis distance
+		 */ 
+		public static double distance(double o1, double o2) {
+				double res = 0;
+				
+				return res;
+		}
+		
+}
