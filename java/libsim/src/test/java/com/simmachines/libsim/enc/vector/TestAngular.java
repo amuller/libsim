7d212d4a82cadb4ba86335809479893274969e5e
jfquesada <chosco34@gmail.com>
2012-09-28 23:14:14 -0600

    Added test case for Angular

--- java/libsim/src/test/java/com/simmachines/libsim/enc/vector/TestAngular.java
+++ java/libsim/src/test/java/com/simmachines/libsim/enc/vector/TestAngular.java
@@ -36,4 +36,44 @@ public class TestAngular {
 		Asserts.assertEquals(res, 1.0387190997679);
 	}
 	
+	@Test
+	//jfquesada
+	public void test4(){
+		double[] v1 = new double[]{5, 8, 4, 1};
+		double[] v2 = new double[]{10, 90, 4, 3};
+		Asserts.assertEqualLength(v1, v2);
+		double res = Angular.distance(v1, v2);
+		Asserts.assertEquals(res, 0.564233312);
+	}
+	
+	@Test
+	//jfquesada
+	public void test5(){
+		double[] v1 = new double[]{-1, 50, -607, -4, 55};
+		double[] v2 = new double[]{0, 47, 0, 48, -4};
+		Asserts.assertEqualLength(v1, v2);
+		double res = Angular.distance(v1, v2);
+		Asserts.assertEquals(res, 1.523689502);
+	}
+	
+	@Test
+	//jfquesada
+	public void test6(){
+		double[] v1 = new double[]{10.233, 37.87, -30.806};
+		double[] v2 = new double[]{11.69, -24.556, 72.158};
+		Asserts.assertEqualLength(v1, v2);
+		double res = Angular.distance(v1, v2);
+		Asserts.assertEquals(res, 2.479339066);
+	}
+	
+	@Test
+	//jfquesada
+	public void test7(){
+		double[] v1 = new double[]{542.479, 709.32, -571.142, 305.37, 4.213};
+		double[] v2 = new double[]{-27.638, -362.329, 278.404, -855.669, -905.833};
+		Asserts.assertEqualLength(v1, v2);
+		double res = Angular.distance(v1, v2);
+		Asserts.assertEquals(res, 2.066171131);
+	}
+	
 }
