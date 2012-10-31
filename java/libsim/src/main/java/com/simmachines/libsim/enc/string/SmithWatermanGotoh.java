9a4f54a5d6bd40a5f4df1257a74534704dc91194
Jose Daniel Salazar Vargas <jdsalazar@jdsalazar-Inspiron-N5010.(none)>
2012-10-31 00:48:56 -0600

    add SmithWatermanGotoh, RASmithWatermanGotoh and TestSmithWatermanGotoh

---
+++ java/libsim/src/main/java/com/simmachines/libsim/enc/string/SmithWatermanGotoh.java
@@ -0,0 +1,60 @@
+package com.simmachines.libsim.enc.string;
+
+import com.simmachines.libsim.common.Transformations;
+
+/**
+ * [Smith-Waterman-Gotoh distance]
+ * <p>
+ * :: The Smith-Waterman-Gotoh distance is a measure between two strings applying an improved version (Gotoh) of the Smith-Waterman algorithm.
+ * <p>
+ * [Brief Description]
+ * <p>
+ * @see com.simmachines.libsim.enc.string.SmithWaterman; 
+ * <p>
+ * [Reference]
+ * <p>
+ * Deza, Michel Marie, Deza, Elena :: [Encyclopedia of Distances] :: Springer |
+ * 2009
+ * <p>
+ * | 11.1 :: Distances on general strings :: P.208 |
+ * <p>
+ * @see <a href="http://en.wikipedia.org/wiki/Smith%E2%80%93Waterman_algorithm">Wikipedia Article</a>
+ * <p>
+ * @see <a href="http://www.gen.tcd.ie/molevol/nwswat.html">Reference by author</a>
+ * 
+ * @author Jose Daniel Salazar-Vargas
+ * 
+ */
+
+
+public class SmithWatermanGotoh {
+
+	/**
+	 * SimMetrics SmithWatermanGotoh object
+	 * @see uk.ac.shef.wit.simmetrics.similaritymetrics.SmithWatermanGotoh;
+	 */
+	public static uk.ac.shef.wit.simmetrics.similaritymetrics.SmithWatermanGotoh smithWatermanGotoh = new uk.ac.shef.wit.simmetrics.similaritymetrics.SmithWatermanGotoh(); 
+	
+	
+	/**
+	 * Receives two integer arrays representing two words and calculates the Smith-Waterman-Gotoh distance.
+	 * @param str1 integer array number 1.
+	 * @param str2 integer array number 2.
+	 * @return The Smith-Waterman-Gotoh distance.
+	 */
+	public static double distance(int[] str1, int[] str2){
+		return smithWatermanGotoh.getSimilarity(Transformations.convertIntArrayToString(str1), Transformations.convertIntArrayToString(str2));
+	}
+	
+	
+	/**
+	 * Receives two strings representing two words and calculates the Smith-Waterman-Gotoh distance.
+	 * @param str1 string number 1.
+	 * @param str2 string number 2.
+	 * @return The Smith-Waterman-Gotoh distance.
+	 */
+	public static double distance(String str1, String str2){
+		return smithWatermanGotoh.getSimilarity(str1, str2);
+	}
+	
+}
---
+++ java/libsim/src/main/java/com/simmachines/libsim/r01/vector/RASmithWatermanGotoh.java
@@ -0,0 +1,59 @@
+package com.simmachines.libsim.r01.vector;
+
+import com.diapai.ramiel.RA;
+import com.simmachines.libsim.enc.string.SmithWatermanGotoh;
+import com.simmachines.libsim.r01.abst.AbstractIntVector;
+
+/**
+ * [Smith-Waterman-Gotoh distance]
+ * <p>
+ * :: The Smith-Waterman-Gotoh distance is a measure between two strings applying an improved version (Gotoh) of the Smith-Waterman algorithm.
+ * <p>
+ * @see com.simmachines.libsim.enc.string.SmithWatermanGotoh;
+ * @author Jose Daniel Salazar-Vargas
+ * 
+ */
+
+
+public class RASmithWatermanGotoh extends AbstractIntVector{
+	/** 
+	 * Default constructor required by R-01
+	 */
+	public RASmithWatermanGotoh(){
+			
+	}
+	
+	/**
+	 * Copy constructor
+	 * @param obj vector to copy.
+	 */ 
+	public RASmithWatermanGotoh(int[] obj){
+			super(obj);
+	}
+	
+	/**
+	 * Parse a vector from a String
+	 * @param line String that contains an encoded version of the object. 
+	 */
+	public RASmithWatermanGotoh(String line){
+			super(line);
+	}
+	
+	
+	/**
+	 * Clone must be implemented by all classes that implement RA. 
+	 * @see java.lang.Object#clone()
+	 */
+	public Object clone() {	
+			return new RASmithWatermanGotoh(cloneHelper());
+	}
+	
+	/** Calculates the Smith-Waterman-Gotoh distance between this and other RASmithWatermanGotoh object
+	 *  @param other RASmithWatermanGotoh object
+	 *  @return Smith-Waterman-Gotoh distance
+	 */ 
+	@Override
+			public Double distance(RA<Double> other) {
+			return SmithWatermanGotoh.distance(getVector(),((RASmithWatermanGotoh)other).getVector());
+	}
+}
