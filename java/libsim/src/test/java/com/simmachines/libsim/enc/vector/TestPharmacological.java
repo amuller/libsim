e25c378243977a4927515f03c78fc06c7c039cfd
fquesada <fquesada@fquesada-desk.(none)>
2012-10-31 16:54:56 -0600

    Added test case for Pharmacological

--- java/libsim/src/test/java/com/simmachines/libsim/enc/vector/TestPharmacological.java
+++ java/libsim/src/test/java/com/simmachines/libsim/enc/vector/TestPharmacological.java
@@ -36,4 +36,64 @@ public class TestPharmacological {
 		Asserts.assertEquals(res, 7420.86983265412896);
 	}
 	
+	@Test
+	//jfquesada
+	public void test4(){
+		double[] v1 = new double[]{0, 0, 0, 0, 0};
+		double[] v2 = new double[]{1, 1, 1, 1, 1};
+		Asserts.assertEqualLength(v1, v2);
+		double res = Pharmacological.distance(v1, v2);
+		Asserts.assertEquals(res, 2.23606797749979);
+	}
+	
+	@Test
+	//jfquesada
+	public void test5(){
+		double[] v1 = new double[]{1, 1, 1, 1, 1};
+		double[] v2 = new double[]{0, 0, 0, 0, 0};
+		Asserts.assertEqualLength(v1, v2);
+		double res = Pharmacological.distance(v1, v2);
+		Asserts.assertEquals(res, 2.23606797749979);
+	}
+	
+	@Test
+	//jfquesada
+	public void test6(){
+		double[] v1 = new double[]{638, 398, 337, 529, 514};
+		double[] v2 = new double[]{689, 869, 383, 706, 620};
+		Asserts.assertEqualLength(v1, v2);
+		double res = Pharmacological.distance(v1, v2);
+		Asserts.assertEquals(res, 518.7706622391054);
+	}
+	
+	@Test
+	//jfquesada
+	public void test7(){
+		double[] v1 = new double[]{-2, 6, -4, -8, 5};
+		double[] v2 = new double[]{-6, 4, -5, -7, 3};
+		Asserts.assertEqualLength(v1, v2);
+		double res = Pharmacological.distance(v1, v2);
+		Asserts.assertEquals(res, 5.0990195135927845);
+	}
+	
+	@Test
+	//jfquesada
+	public void test8(){
+		double[] v1 = new double[]{19415.212429584662, 191257.52282304363, 913239.9167389673, 566196.942598401, 153677.14241202734};
+		double[] v2 = new double[]{208269.3339067423, 664207.5606130534, 76527.14625467494, 767410.7016052309, 602059.2951081641};
+		Asserts.assertEqualLength(v1, v2);
+		double res = Pharmacological.distance(v1, v2);
+		Asserts.assertEquals(res, 1095887.4985170974);
+	}
+	
+	@Test
+	//jfquesada
+	public void test9(){
+		double[] v1 = new double[]{9.629962536879205E8, -1.6826823387640044E8, -1.172045780576203E8, 6.409943660337736E7, 3.0154528518528813E8};
+		double[] v2 = new double[]{3.2714922830083424E8, 5.708280820465017E7, -6.131738568306583E8, -6.433943832389486E8, 8.06844862750207E8};
+		Asserts.assertEqualLength(v1, v2);
+		double res = Pharmacological.distance(v1, v2);
+		Asserts.assertEquals(res, 1.2070398607956738E9);
+	}
+	
 }
