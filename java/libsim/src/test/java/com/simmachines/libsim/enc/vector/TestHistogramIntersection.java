289f5fab85f5a408fb8837dce902604d13836498
fquesada <fquesada@fquesada-desk.(none)>
2012-10-31 10:29:52 -0600

    Added testcases to HistogramIntersection

--- java/libsim/src/test/java/com/simmachines/libsim/enc/vector/TestHistogramIntersection.java
+++ java/libsim/src/test/java/com/simmachines/libsim/enc/vector/TestHistogramIntersection.java
@@ -42,4 +42,64 @@ public class TestHistogramIntersection {
 		Asserts.assertEquals(res, 0.42125550660793);
 	}
 	
+	@Test
+	//jfquesada
+	public void test4(){
+		int[] v1 = new int[]{0, 0, 0, 0, 0};
+		int[] v2 = new int[]{1, 1, 1, 1, 1};
+		Asserts.assertEqualLength(v1, v2);
+		Asserts.assertPositiveValues(v1);
+		Asserts.assertPositiveValues(v2);
+		double res = HistogramIntersection.distance(v1, v2);
+		Asserts.assertEquals(res, 1.0);
+	}
+	
+	@Test
+	//jfquesada
+	public void test5(){
+		int[] v1 = new int[]{1, 1, 1, 1, 1};
+		int[] v2 = new int[]{0, 0, 0, 0, 0};
+		Asserts.assertEqualLength(v1, v2);
+		Asserts.assertPositiveValues(v1);
+		Asserts.assertPositiveValues(v2);
+		double res = HistogramIntersection.distance(v1, v2);
+		Asserts.assertEquals(res, 1.0);
+	}
+	
+	@Test
+	//jfquesada
+	public void test6(){
+		int[] v1 = new int[]{638, 398, 337, 529, 514};
+		int[] v2 = new int[]{689, 869, 383, 706, 620};
+		Asserts.assertEqualLength(v1, v2);
+		Asserts.assertPositiveValues(v1);
+		Asserts.assertPositiveValues(v2);
+		double res = HistogramIntersection.distance(v1, v2);
+		Asserts.assertEquals(res, 0.0);
+	}
+	
+	@Test
+	//jfquesada
+	public void test7(){
+		int[] v1 = new int[]{1541367828, 950213225, 52610973, 1431957942, 452032515};
+		int[] v2 = new int[]{1572602206, 598716249, 536608579, 84359194, 2018058772};
+		Asserts.assertEqualLength(v1, v2);
+		Asserts.assertPositiveValues(v1);
+		Asserts.assertPositiveValues(v2);
+		double res = HistogramIntersection.distance(v1, v2);
+		Asserts.assertEquals(res, 0.3837004754259582);
+	}
+	
+	@Test
+	//jfquesada
+	public void test8(){
+		int[] v1 = new int[]{1141454369, 589096141, 922770477, 822465917, 423607600};
+		int[] v2 = new int[]{247243949, 375221325, 851955869, 530827397, 1139764455};
+		Asserts.assertEqualLength(v1, v2);
+		Asserts.assertPositiveValues(v1);
+		Asserts.assertPositiveValues(v2);
+		double res = HistogramIntersection.distance(v1, v2);
+		Asserts.assertEquals(res, 0.3771196688335898);
+	}
+	
 }
