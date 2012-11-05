5e8ca5ec0292bf70dbe927ba26cc11784e05da39
Jose Daniel Salazar Vargas <jdsalazar@jdsalazar-Inspiron-N5010.(none)>
2012-11-05 00:17:33 -0600

    add TestChapmanLengthDeviation, TestChapmanMatchingSoundex, TestChapmanMeanLength and TestChapmanOrderedNameCompoundSimilarity

---
+++ java/libsim/src/test/java/com/simmachines/libsim/enc/string/TestChapmanOrderedNameCompoundSimilarity.java
@@ -0,0 +1,18 @@
+package com.simmachines.libsim.enc.string;
+
+import org.junit.Test;
+
+import com.simmachines.libsim.asserts.Asserts;
+
+public class TestChapmanOrderedNameCompoundSimilarity {
+
+	@Test
+	//jdsalazar
+	public void test1(){
+		String str1 = "Test String1";
+		String str2 = "Test String2";
+		double res = ChapmanOrderedNameCompoundSimilarity.distance(str1,str2);
+		Asserts.assertEquals(res, 0.9553572);
+	}
+	
+}
