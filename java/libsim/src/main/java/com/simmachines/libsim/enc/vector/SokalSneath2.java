452c82ce2796214eaf98dafc68da5e250cd315d7
Jose Daniel Salaar Vargas <jdsalazar@ASUS.jdsalazar>
2013-01-03 20:23:18 -0600

    add SokalSneath2, RASokalSneath2 and TestSokalSneath2

---
+++ java/libsim/src/main/java/com/simmachines/libsim/enc/vector/SokalSneath2.java
@@ -0,0 +1,51 @@
+package com.simmachines.libsim.enc.vector;
+
+import com.simmachines.libsim.asserts.Asserts;
+import com.simmachines.libsim.common.CommonStats;
+
+/**
+ * [Sokal-Sneath 2 distance]
+ * <p>
+ * :: The Sokal-Sneath 2 distance is a distance for binary data on \(\{0,1\}^n\).
+ * <p>
+ * [Definition]
+ * <p>
+ 
+ 
+ The Sokal-Sneath 2 distance between two binary vectors \(A,B\) of length \(n\) is given by:
+ <p>
+ \( d = 1 - \frac{|A\cap B|}{|A\cup B|+|A\Delta B|} \)
+ 
+ 
+ * 
+ * <p>
+ * [Reference]
+ * <p>
+ * Deza, Michel Marie, Deza, Elena :: [Encyclopedia of Distances] :: Springer |
+ * 2009
+ * <p>
+ * | 17.3 :: Similarities and distances for binary data :: P.304 |
+ * <p>
+ * 
+ * @author Jose Daniel Salazar-Vargas
+ * 
+ */
+
+
+public class SokalSneath2 {
+
+	/**
+	 * Received two binary vectors and calculates the Sokal-Sneath 2 distance.
+	 * @param v1 binary vector number 1.
+	 * @param v2 binary vector number 2.
+	 * @return The Sokal-Sneath 2 distance.
+	 */
+	public static double distance(int[] v1, int[] v2){
+		Asserts.assertEqualLength(v1, v2);
+		Asserts.assertIsBinary(v1);
+		Asserts.assertIsBinary(v2);
+		int[] matches = CommonStats.processBinaryVectors(v1, v2);
+		return 1-((double)(matches[0])/(double)(matches[0]+(2*matches[1])+(2*matches[2])));
+	}
+	
+}
---
+++ java/libsim/src/main/java/com/simmachines/libsim/r01/vector/RASokalSneath2.java
@@ -0,0 +1,59 @@
+package com.simmachines.libsim.r01.vector;
+
+import com.diapai.ramiel.RA;
+import com.simmachines.libsim.enc.vector.SokalSneath2;
+import com.simmachines.libsim.r01.abst.AbstractIntVector;
+
+/**
+ * [Sokal-Sneath 2 distance]
+ * <p>
+ * :: The Sokal-Sneath 2 distance is a distance for binary data on \(\{0,1\}^n\).
+ * <p>
+ * @see com.simmachines.libsim.enc.vector.SokalSneath2;
+ * @author Jose Daniel Salazar-Vargas
+ */
+
+
+public class RASokalSneath2 extends AbstractIntVector{
+	/** 
+	 * Default constructor required by R-01
+	 */
+	public RASokalSneath2(){
+			
+	}
+	
+	/**
+	 * Copy constructor
+	 * @param obj vector to copy.
+	 */ 
+	public RASokalSneath2(int[] obj){
+			super(obj);
+	}
+	
+	/**
+	 * Parse a vector from a String
+	 * @param line String that contains an encoded version of the object. 
+	 */
+	public RASokalSneath2(String line){
+			super(line);
+	}
+	
+	
+	/**
+	 * Clone must be implemented by all classes that implement RA. 
+	 * @see java.lang.Object#clone()
+	 */
+	public Object clone() {	
+			return new RASokalSneath2(cloneHelper());
+	}
+	
+	/** Calculates the Sokal-Sneath 2 distance between this and other RASokalSneath2 object
+	 *  @param other RASokalSneath2 object
+	 *  @return Sokal-Sneath 2 distance
+	 */ 
+	@Override
+			public Double distance(RA<Double> other) {
+			return SokalSneath2.distance(getVector(),((RASokalSneath2)other).getVector());
+	}
+
+}
